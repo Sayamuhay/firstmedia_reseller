bfabd12f914257aa4eb7286110810b51f54927ac
Sayamuhay <sayamuhay@gmail.com>
2022-11-02 11:17:45 +0700

    third commit

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -7,7 +7,7 @@ import { createInertiaApp } from '@inertiajs/inertia-react';
 import { InertiaProgress } from '@inertiajs/progress';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
-const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'First Media Reseller';
+const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,8 @@ Route::get('/', function () {
     ]);
 });
 
-Route::inertia('/legal-disclaimer', 'LegalDisclaimer');
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/auth.php';
